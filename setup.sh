648e47b86300bcf9693ff15276e3edd255987422
Nick Girardo <nickgirardo@gmail.com>
2021-04-09 17:28:57 -0400

    Install mods separately, install hornpack

--- setup.sh
+++ setup.sh
@@ -18,21 +18,29 @@ else
     sudo apt-get -y install srb2kart
 fi
 
+# Install mods
 
-# Check if mods are installed
+# Check if kartserv.cfg exists
+# kartserv.cfg is read on startup
+# It must be located in /root/.srb2kart/kartserv.cfg (assuming running as root)
 if test -f /root/.srb2kart/kartserv.cfg
 then
-    echo "kartserv.cfg found, skipping mod installation"
+    echo "kartserv.cfg exists, skipping creation"
 else
-    # Install mods
-
-    # Create kartserv.cfg
-    # kartserv.cfg is read on startup
-
     # This directory would be automatically created on first run of srb2kart
     # But we need to put stuff in it before first boot
-    mkdir /root/.srb2kart/
-    # It must be located in /root/.srb2kart/kartserv.cfg (assuming running as root)
+    mkdir -p /root/.srb2kart/
+    # Just creating a placeholder for now, will check for contents later
+    touch /root/.srb2kart/kartserv.cfg
+fi
+
+
+# Install hornmod
+if grep -q "addfile hornmod.pk3" /root/.srb2kart/kartserv.cfg; then
+    echo "hornmod enabled, skipping download"
+else
+    # Set the contents of kartserv.cfg to the following string
+    # It is important that this mod comes first, all other mods must be appended after
     echo "addfile hornmod.pk3" > /root/.srb2kart/kartserv.cfg
 
     # Download hornmod.pk3
@@ -40,8 +48,20 @@ else
     curl https://www.nickgirardo.com/hornmod.pk3 > /usr/games/SRB2Kart/hornmod.pk3
 fi
 
-# TODO install custom horns
-# TODO update config file with custom horns
+
+# Install hornpack (custom horns for hornmod)
+# TODO maybe check hornmod is installed here as that must happen first?
+if grep -q "addfile hornpack.pk3" /root/.srb2kart/kartserv.cfg; then
+    echo "hornpack enabled, skipping download"
+else
+    # Appending this mod
+    # It relies on hornmod and must come afterwards
+    echo "addfile hornpack.pk3" >> /root/.srb2kart/kartserv.cfg
+
+    # Download hornpack.pk3
+    # TODO take as env var?
+    curl https://www.nickgirardo.com/hornpack.pk3 > /usr/games/SRB2Kart/hornpack.pk3
+fi
 
 # Start server
 srb2kart -dedicated
