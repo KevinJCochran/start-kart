961667be93dd3ea8db270fdb3c7304aaf1535b0c
Nick Girardo <nickgirardo@gmail.com>
2021-04-09 14:13:08 -0400

    Updated main.go: Use dotenv for secrets

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module srb2kart
 
 go 1.15
 
-require github.com/digitalocean/godo v1.57.0
+require (
+	github.com/digitalocean/godo v1.57.0
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"os"
+	"log"
 	"context"
-	"fmt"
 	"github.com/digitalocean/godo"
+	"github.com/joho/godotenv"
 )
 
-var sshFingerprint = ""
-var token = ""
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("DO_TOKEN")
+
 	client := godo.NewFromToken(token)
 	ctx := context.TODO()
 
 	newDroplet := createDroplet(client, ctx)
 
-	fmt.Printf("New droplet: %v", newDroplet)
+	log.Printf("New droplet: %v", newDroplet)
 }
 
 func createDroplet(client *godo.Client, ctx context.Context) *godo.Droplet {
+	sshFingerprint := os.Getenv("DO_SSH_FINGERPRINT")
+
 	workKey := godo.DropletCreateSSHKey{Fingerprint: sshFingerprint}
 	createRequest := &godo.DropletCreateRequest{
 		Name:   "srb2kart-server",
@@ -34,7 +42,7 @@ func createDroplet(client *godo.Client, ctx context.Context) *godo.Droplet {
 
 	droplet, _, err := client.Droplets.Create(ctx, createRequest)
 	if err != nil {
-		fmt.Printf("Unable to create droplet: %v", err.Error())
+		log.Printf("Unable to create droplet: %v", err.Error())
 	}
 
 	return droplet
